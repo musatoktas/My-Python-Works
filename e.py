d8bc0387e215197a5638e8c7300930f9d3b65496
musatoktas <mdeepst@gmail.com>
2015-02-19 17:30:27 -0600

    First//

---
+++ e.py
@@ -0,0 +1,51 @@
+{
+if x>0: 
+	print(“poditive nuber”)
+elif x < 0:
+print(“ negative number”)
+else: 
+print(“zero”)
+}
+
+{
+while True:
+	print(n, end = " ")
+	n -= 1
+}
+
+{
+while True :
+	line = input("Write Something")
+	if line == "done":
+		break
+	print(line)
+
+
+
+
+{
+while True:
+	line = input("Write Something")
+	if line[0] == "#":
+		continue
+	if line == "done":
+		break
+	print(line)
+}
+
+{
+for i in "Python" :
+	print (i)
+}karsina bole altalta yazmis halde cikcak harfler
+{
+for i in range(10):
+	for j in range(5):
+		print("*")
+}
+{
+for i in list_:
+	if i == 5:
+		continue
+	print(i)
+}if e yazdigin rakami ihmal ediyor
+
