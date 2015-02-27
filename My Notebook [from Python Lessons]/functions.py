26b9b59797660ffa3fb9e862bce4638e6988c3e9
musatoktas <mdeepst@gmail.com>
2015-02-26 21:54:08 -0600

    the fundamentals of functions.

---
+++ My Notebook [from Python Lessons]/functions.py
@@ -0,0 +1,22 @@
+def function_name():
+    print("Hello World")
+
+def funtion2(i, j):
+    return i+j
+
+def average(a,b,c,d):
+    return (a+b+c+d)/4
+
+def list_average(list):
+    return(sum(list)/len(list))
+
+def convert_to_celcius(fahrenheit):
+    return (fahrenheit-32)*5/9
+def convert_to_fahrenheit(celcius):
+    return (celcius*9)/5+32
+
+def quadratic(a,b,c,x):
+    first = a* x ** 2
+    second = b*x
+    third = c
+    return first + second + third
