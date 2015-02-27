c32316d127c0bc6267ecc7cf83417362c1ec9630
musatoktas <mdeepst@gmail.com>
2015-02-26 21:56:04 -0600

    There are application of the functions.

---
+++ My Notebook [from Python Lessons]/applications_of_functions.py
@@ -0,0 +1,74 @@
+>>> ================================ RESTART ================================
+>>> 
+>>> funtion_name()
+Traceback (most recent call last):
+  File "<pyshell#50>", line 1, in <module>
+    funtion_name()
+NameError: name 'funtion_name' is not defined
+>>> function_name()
+Hello World
+>>> ================================ RESTART ================================
+>>> 
+>>> function2()
+Traceback (most recent call last):
+  File "<pyshell#52>", line 1, in <module>
+    function2()
+NameError: name 'function2' is not defined
+>>> ================================ RESTART ================================
+>>> 
+>>> function2(3,4)
+Traceback (most recent call last):
+  File "<pyshell#53>", line 1, in <module>
+    function2(3,4)
+NameError: name 'function2' is not defined
+>>> ================================ RESTART ================================
+>>> 
+>>> ================================ RESTART ================================
+>>> 
+>>> function2(3,5)
+Traceback (most recent call last):
+  File "<pyshell#54>", line 1, in <module>
+    function2(3,5)
+NameError: name 'function2' is not defined
+>>> ================================ RESTART ================================
+>>> 
+>>> ================================ RESTART ================================
+>>> 
+>>> list_average(list)
+Traceback (most recent call last):
+  File "<pyshell#55>", line 1, in <module>
+    list_average(list)
+  File "C:/Users/musa/Desktop/hello.py", line 11, in list_average
+    return(sum(list)/list.count)
+TypeError: 'type' object is not iterable
+>>> list= (1,2,3,4)
+>>> list_average(list)
+Traceback (most recent call last):
+  File "<pyshell#57>", line 1, in <module>
+    list_average(list)
+  File "C:/Users/musa/Desktop/hello.py", line 11, in list_average
+    return(sum(list)/list.count)
+TypeError: unsupported operand type(s) for /: 'int' and 'builtin_function_or_method'
+>>> ================================ RESTART ================================
+>>> 
+>>> list_average(list)
+Traceback (most recent call last):
+  File "<pyshell#58>", line 1, in <module>
+    list_average(list)
+  File "C:/Users/musa/Desktop/hello.py", line 11, in list_average
+    return(sum(list)/len.list)
+TypeError: 'type' object is not iterable
+>>> ================================ RESTART ================================
+>>> 
+>>> list= (1,2,3,4)
+>>> list_average(list)
+2.5
+>>> ================================ RESTART ================================
+>>> 
+>>> convert_to_fahrenheit(12)
+79.2
+>>> ================================ RESTART ================================
+>>> 
+>>> quadratic(1,2,3,4)
+27
+>>> 
